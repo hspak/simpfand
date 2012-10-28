77b04e5d1bd1a5a12c30371c6c7d6a36b800958d
Hong Shick Pak <hongshick.pak@gmail.com>
2012-10-28 02:40:14 -0400

    fixed tab space

--- options.h
+++ options.h
@@ -1,10 +1,10 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
-#define OPT_HELP  1
-#define OPT_VERSION   2
+#define OPT_HELP    1
+#define OPT_VERSION 2
 #define OPT_START   3
-#define OPT_STOP  4
+#define OPT_STOP    4
 
 int read_command(int argc, char *argv[]);
 int module_enabled(char *path, char *mode);
--- parse.h
+++ parse.h
@@ -1,53 +1,51 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-#define INC_LOW_TEMP	55
-#define INC_HIGH_TEMP	65
-#define INC_MAX_TEMP	82
+#define INC_LOW_TEMP    55
+#define INC_HIGH_TEMP   65
+#define INC_MAX_TEMP    82
 
-#define BASE_LEVEL	1
-#define INC_LOW_LEVEL	2
-#define INC_HIGH_LEVEL	4
-#define INC_MAX_LEVEL	6
+#define BASE_LEVEL      1
+#define INC_LOW_LEVEL   2
+#define INC_HIGH_LEVEL  4
+#define INC_MAX_LEVEL   6
 
-#define DEC_LOW_LEVEL	4
-#define DEC_HIGH_LEVEL	5
-#define DEC_MAX_LEVEL	6
+#define DEC_LOW_LEVEL   4
+#define DEC_HIGH_LEVEL  5
+#define DEC_MAX_LEVEL   6
 
-#define DEC_THRESH	0
+#define DEC_LOW_TEMP    50
+#define DEC_HIGH_TEMP   60
+#define DEC_MAX_TEMP    77
 
-#define DEC_LOW_TEMP	50
-#define DEC_HIGH_TEMP	60
-#define DEC_MAX_TEMP	77
+#define MAX_FAN_LVL     7
 
-#define MAX_FAN_LVL	7
+#define POLL_INTERVAL   10
+#define BUFF_MAX        128
+#define PATH_MAX        128
 
-#define POLL_INTERVAL	10
-#define BUFF_MAX	128
-#define PATH_MAX	128
-
-#define STR_STARTS_WITH(x,y)	(strncmp((x),(y), strlen(y)) == 0)
+#define STR_STARTS_WITH(x,y)  (strncmp((x),(y), strlen(y)) == 0)
 
 struct config {
-	unsigned short inc_low_temp;
-	unsigned short inc_high_temp;
-	unsigned short inc_max_temp;
+  unsigned short inc_low_temp;
+  unsigned short inc_high_temp;
+  unsigned short inc_max_temp;
 
-	unsigned short inc_low_lvl;
-	unsigned short inc_high_lvl;
-	unsigned short inc_max_lvl;
+  unsigned short inc_low_lvl;
+  unsigned short inc_high_lvl;
+  unsigned short inc_max_lvl;
 
-	unsigned short dec_low_temp;
-	unsigned short dec_high_temp;
-	unsigned short dec_max_temp;
+  unsigned short dec_low_temp;
+  unsigned short dec_high_temp;
+  unsigned short dec_max_temp;
 
-	unsigned short dec_low_lvl;
-	unsigned short dec_high_lvl;
-	unsigned short dec_max_lvl;
+  unsigned short dec_low_lvl;
+  unsigned short dec_high_lvl;
+  unsigned short dec_max_lvl;
 
-	unsigned short base_lvl;
-	unsigned short poll_int;
-	unsigned short max_temp;
+  unsigned short base_lvl;
+  unsigned short poll_int;
+  unsigned short max_temp;
  };
 
 size_t strtrim(char *str);
