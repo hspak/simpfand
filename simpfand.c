95154d9d6f85c650180894ce5317b9c9736162d9
Hong Shick Pak <hongshick.pak@gmail.com>
2012-10-29 22:04:08 -0400

    no longer uses 'system' to change levels

--- simpfand.c
+++ simpfand.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "parse.h"
 #include "options.h"
 
-#define CMD_MAX 36
 #define SET_TEMP 0
 #define MAX_TEMP 1
+#define LVL_LEN 8
 
 void die(char *msg, int exit_code)
 {
@@ -40,10 +43,10 @@ unsigned short get_temp(int type)
         FILE *fp;
         unsigned int read_temp;
 
-        if (type == MAX_TEMP)
-                fp = fopen("/sys/devices/platform/coretemp.0/temp1_max", "r");
-        else
+        if (type == SET_TEMP)
                 fp = fopen("/sys/devices/platform/coretemp.0/temp1_input", "r");
+        else
+                fp = fopen("/sys/devices/platform/coretemp.0/temp1_max", "r");
 
         if (!fp) die("error: could not read temperature input", EXIT_FAILURE);
         fscanf(fp, "%u", &read_temp);
@@ -52,42 +55,41 @@ unsigned short get_temp(int type)
         return (unsigned short)(read_temp / 1000.);
 }
 
-int get_level(char* cmd, unsigned short old_temp, unsigned short new_temp,
+void get_level(char *level, unsigned short old_temp, unsigned short new_temp,
                struct config *cfg)
 {
         unsigned short temp_diff = new_temp - old_temp;
-        if (temp_diff == 0)
-                return 0;
 
         if (temp_diff <= 0)
                 if (new_temp > cfg->dec_max_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->dec_max_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->dec_max_lvl);
                 else if (new_temp > cfg->dec_high_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->dec_high_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->dec_high_lvl);
                 else if (new_temp > cfg->dec_low_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->dec_low_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->dec_low_lvl);
                 else
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->base_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->base_lvl);
         else
                 if (new_temp <= cfg->inc_low_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->base_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->base_lvl);
                 else if (new_temp <= cfg->inc_high_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->inc_low_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->inc_low_lvl);
                 else if (new_temp <= cfg->inc_max_temp)
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->inc_high_lvl);
+                        snprintf(level, LVL_LEN, "level %d", cfg->inc_high_lvl);
                 else
-                        sprintf(cmd, "echo level %d > /proc/acpi/ibm/fan", cfg->inc_max_lvl);
-        return 1;
+                        snprintf(level, LVL_LEN, "level %d", cfg->inc_max_lvl); 
 }
 
 int main(int argc, char *argv[])
 {
         unsigned short old_temp, new_temp;
-        char cmd[CMD_MAX];
+        char *fan_path = "/proc/acpi/ibm/fan";
+        char level[LVL_LEN];
         struct config cfg;
         int action;
+        int file;
 
-        if (!module_enabled("/proc/acpi/ibm/fan", "r") || !arg_count(argc))
+        if (!module_enabled(fan_path, "r") || !arg_count(argc))
                 return EXIT_FAILURE;
 
         if ((action = read_command(argc, argv)) != 0) {
@@ -95,25 +97,25 @@ int main(int argc, char *argv[])
                         print_help();
                 } else if (action == OPT_VERSION) {
                         print_version();
-                } else if (action == OPT_STOP) {
+                } else if (action == OPT_STOP) {       
                         die("stopping simpfand", EXIT_SUCCESS);
                 } else if (action == OPT_START) {
-                        cfg.max_temp = (unsigned short)get_temp(MAX_TEMP);
+                        cfg.max_temp = get_temp(MAX_TEMP);
                         set_defaults(&cfg);
                         parse_config(&cfg);
                         new_temp = get_temp(SET_TEMP);
 
-                        /*
-                         * initial call because the first call
-                         * in the loop will never happen
-                         */
-                        get_level(cmd, new_temp-1, new_temp, &cfg);
-                        system(cmd);
-
                         while (1) {
                                 old_temp = new_temp;
                                 new_temp = get_temp(SET_TEMP);
-                                if (get_level(cmd, old_temp, new_temp, &cfg)) system(cmd);
+                                get_level(level, old_temp, new_temp, &cfg);
+
+                                if((file = open(fan_path, O_WRONLY)) == -1)
+                                        die("could not open fan file", EXIT_FAILURE);
+                                if((write(file, level, strlen(level))) == -1)
+                                        die("could not write to fan file", EXIT_FAILURE);
+
+                                close(file);
                                 sleep(cfg.poll_int);
                          }
                 }
